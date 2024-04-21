016e81bd56d2fec1e92dd34545353de9317fec9a
Michael Chavarria Flores <maikolchf@outlook.com>
2024-04-20 20:12:44 -0600

    tesla-shop

--- primeros-pasos/app/(general)/usuarios/page.tsx
+++ primeros-pasos/app/(general)/usuarios/page.tsx
@@ -9,12 +9,12 @@ export default function UsuarioPage() {
     return (
         <div className="px-4 sm:px-6 lg:px-8">
             <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto">
+                <div className=" mt-4 sm:flex-auto">
                     <h1 className="text-base font-semibold leading-6 text-gray-900"></h1>
                     <p className="mt-2 text-sm text-gray-700">Lista de usuarios registrados en el sistema</p>
                 </div>
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <button type="button" className="block rounded-md bg-balck px-3 py-2 text-center text-sm font-semibold text-black shadow-sm hover:text-white hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">Agregar usuario</button>
+                <div className=" mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button type="button" className=" mt-4 block rounded-md bg-balck px-3 py-2 text-center text-sm font-semibold text-black shadow-sm hover:text-white hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">Agregar usuario</button>
                 </div>
             </div>
             <div className="mt-8 flow-root">
