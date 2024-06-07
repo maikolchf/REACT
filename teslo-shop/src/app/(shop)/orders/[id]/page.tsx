a2bf407326b2012ea87f3e4699d4fa47ba9b1216
Michael <maikolchf@outlook.com>
2024-06-06 18:04:40 -0600

    Se agrega nueva funcionalidad de ontener ordenes

--- teslo-shop/src/app/(shop)/orders/[id]/page.tsx
+++ teslo-shop/src/app/(shop)/orders/[id]/page.tsx
@@ -3,6 +3,9 @@ import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import clsx from "clsx";
 import { IoCardOutline } from "react-icons/io5";
+import { getOrderById } from "@/actions";
+import { redirect } from "next/navigation";
+import { currencyFormat } from '../../../../utils/currencyFormat';
 
 const productsInCart = [
   initialData.products[0],
@@ -16,8 +19,14 @@ interface Props {
   };
 }
 
-export default function OrderIdPage({ params }: Props) {
+export default async function OrderIdPage({ params }: Props) {
   const { id } = params;
+
+  
+  const result = await getOrderById(id);
+
+  if(!result.ok) return redirect("/")
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
@@ -30,70 +39,68 @@ export default function OrderIdPage({ params }: Props) {
               className={clsx(
                 "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
                 {
-                  "bg-red-500": false,
-                  "bg-green-700": true,
+                  "bg-red-500": !result.order?.isPaid,
+                  "bg-green-700": result.order?.isPaid,
                 }
               )}
             >
               <IoCardOutline size={30} />
-              {/* <span className="mx-2">Pendiente de pago</span> */}
-              <span className="mx-2">Pagada</span>
+              <span className="mx-2">{ !result.order?.isPaid ? "Pendiente de pago" : "Pagada" }</span>
             </div>
 
             {/* Items */}
-            {productsInCart.map((product) => (
-              <div key={product.slug} className="flex mb-5">
+            {result.order?.OrderItem?.map((product) => (
+              <div key={product.product.slug} className="flex mb-5">
                 <Image
-                  src={`/products/${product.images[0]}`}
+                  src={`/products/${product.product.ProductImage[0].url}`}
                   width={100}
                   height={100}
                   style={{
                     width: "100px",
                     height: "100px",
                   }}
-                  alt={product.title}
+                  alt={product.product.title}
                   className="mr-5 rounded"
                 />
 
                 <div>
-                  <p>{product.title}</p>
-                  <p>${product.price} x 3</p>
-                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  <p>{product.product.title}</p>
+                  <p>{currencyFormat(product.price)} x {product.quantity}</p>
+                  <p className="font-bold">Subtotal: {currencyFormat(product.price * product.quantity)}</p>
                 </div>
               </div>
             ))}
           </div>
-
-          {/* Checkout - Resumen de orden */}
           <div className="bg-white rounded-xl shadow-xl p-7">
             <h2 className="text-2xl mb-2">Dirección de entrega</h2>
             <div className="mb-10">
-              <p className="text-xl">Fernando Herrera</p>
-              <p>Av. Siempre viva 123</p>
-              <p>Col. Centro</p>
-              <p>Alcaldía Cuauhtémoc</p>
-              <p>Ciudad de México</p>
-              <p>CP 123123</p>
-              <p>123.123.123</p>
+            <p className="text-xl">{result.order?.OrderAddress?.firstName} {result.order?.OrderAddress?.lastName}</p>
+                <p>{result.order?.OrderAddress?.address}</p>
+                <p>{result.order?.OrderAddress?.address2}</p>
+                <p>{result.order?.OrderAddress?.postalCode}</p>
+                <p>{result.order?.OrderAddress?.city}, {result.order?.OrderAddress?.countryId}</p>
+                <p>{result.order?.OrderAddress?.phone}</p>
             </div>
-
-            {/* Divider */}
             <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
 
             <h2 className="text-2xl mb-2">Resumen de orden</h2>
 
             <div className="grid grid-cols-2">
               <span>No. Productos</span>
-              <span className="text-right">3 artículos</span>
+              <span className="text-right">{ 
+                (result.order?.itemsInOrder ?? 0) < 1 ? 
+                `${result.order?.itemsInOrder} artículo` : 
+                `${result.order?.itemsInOrder} artículos`
+                } </span>
 
               <span>Subtotal</span>
-              <span className="text-right">$ 100</span>
+              <span className="text-right">{ currencyFormat(result.order?.subTotal ?? 0)}</span>
 
               <span>Impuestos (15%)</span>
-              <span className="text-right">$ 100</span>
+              <span className="text-right">{currencyFormat(result.order?.tax ?? 0)}</span>
 
               <span className="mt-5 text-2xl">Total:</span>
-              <span className="mt-5 text-2xl text-right">$ 100</span>
+              <span className="mt-5 text-2xl text-right">{currencyFormat(result.order?.total ?? 0)}</span>
             </div>
 
             <div className="mt-5 mb-2 w-full">
@@ -101,14 +108,13 @@ export default function OrderIdPage({ params }: Props) {
                 className={clsx(
                   "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
                   {
-                    "bg-red-500": false,
-                    "bg-green-700": true,
+                    "bg-red-500": !result.order?.isPaid,
+                    "bg-green-700": result.order?.isPaid,
                   }
                 )}
               >
-                <IoCardOutline size={30} />
-                {/* <span className="mx-2">Pendiente de pago</span> */}
-                <span className="mx-2">Pagada</span>
+                <IoCardOutline size={30} />                
+                <span className="mx-2">{ !result.order?.isPaid ? "Pendiente de pago" : "Pagada" }</span>
               </div>
             </div>
           </div>
--- teslo-shop/src/components/ui/side-bar/Sidebar.tsx
+++ teslo-shop/src/components/ui/side-bar/Sidebar.tsx
@@ -23,7 +23,7 @@ export const Sidebar = () => {
 
   const { data: session } = useSession();
   const isAuthenticated = !!session?.user;
-  const isAdmin = session?.user.role === "admin";
+  const isAdmin = session?.user?.role === "admin";
 
   return (
     <div>
@@ -79,7 +79,7 @@ export const Sidebar = () => {
             </Link>
 
             <Link
-              href="/"
+              href="/orders"
               className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
             >
               <IoTicketOutline size={30} />
